ab92523932f2d6ebc2706d56090cc7483e614701
stephenyong2005 <stephenyong2005@hotmail.com>
2019-06-05 17:12:21 +0800

    modifying the soure code in hello.go with adding some exercise code such as function, struct and iota etc.

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,8 +4,57 @@ import (
 	"fmt"
 
 	"stringutil"
+	"strconv"
 )
+
+type Person struct {
+	Name string
+	Age int
+	Address string
+}
 
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
+	const (
+		a = 10
+		b
+		c
+		d
+	)
+
+	fmt.Println(a,b,c,d)
+
+
+
+	var person Person
+	person.Address = "Orlando, FL USA"
+	person.Age = 25
+	person.Name = "Stephen"
+
+	fmt.Println(person.Name+" at "+strconv.Itoa(person.Age)+" years old, and he comes from "+person.Address)
+
+	var s = 200
+
+	p := &s
+	fmt.Println(*p)
+
+	person.AgePlus(5)
+
+	fmt.Println(person.Name+" at "+strconv.Itoa(person.Age)+" years old, and he comes from "+person.Address)
+
+
+
+
+
+}
+
+func (p *Person) AgePlus(n int) int {
+
+
+	p.Age = p.Age + n
+	fmt.Println(p.Age)
+
+	return 0
+
 }
